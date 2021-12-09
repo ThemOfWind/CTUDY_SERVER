7c0789919bf75b2b61aec66b89ecb800913b786d
mint <pb1123love@gmail.com>
2021-12-09 22:57:31 +0900

    room config 모델 추가

--- backend/room/models.py
+++ backend/room/models.py
@@ -5,5 +5,9 @@ from account.models import Member
 
 class Room(models.Model):
     name = models.CharField(max_length=50)
-    master = models.ForeignKey(Member, on_delete=models.SET_NULL)
     members = models.ManyToManyField(Member)
+
+
+class RoomConfig(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    master = models.ForeignKey(Member, on_delete=models.SET_NULL, null=True)
