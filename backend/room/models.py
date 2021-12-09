a7043f47e1a16bf652716e45171e76412db9b4ef
mint <pb1123love@gmail.com>
2021-12-09 22:54:44 +0900

    스터디룸 모델 생성

--- backend/room/models.py
+++ backend/room/models.py
@@ -4,5 +4,6 @@ from account.models import Member
 
 
 class Room(models.Model):
-    name = models.CharField(max_length=50),
+    name = models.CharField(max_length=50)
+    master = models.ForeignKey(Member, on_delete=models.SET_NULL)
     members = models.ManyToManyField(Member)
