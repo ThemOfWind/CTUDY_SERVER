194dfd3460fd6afebb9d66ce0d759e9b7d326234
MINT <pb1123love@gmail.com>
2022-03-29 22:55:40 +0900

    setting change

--- backend/ctudy_server/settings.py
+++ backend/ctudy_server/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m&=f82t@_w+w0z2(8(lc1=3)om6^3=8f3kf_qqljto@acbp=ra'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
