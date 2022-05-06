b406e372eda740a1dc57b90d2d4eeeab5f365b57
minkyu <pb1123love@gmail.com>
2022-05-06 17:35:19 +0900

    server.sh update

--- backend_v2/server.sh
+++ backend_v2/server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gunicorn --bind 0.0.0.0:8888 his_doc_setting.asgi:application -k uvicorn.workers.UvicornWorker -w 8 &
+gunicorn --bind 0.0.0.0:8888 settings.asgi:application -k uvicorn.workers.UvicornWorker -w 8 &
 nginx
 
